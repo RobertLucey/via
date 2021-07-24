efa6db83cdbef912df709c879aabb698bb44290c
Robert Lucey <robert@glimpse.ie>
2021-07-24 17:52:25 +0100

    Log to tmp if testing

--- src/bike/constants.py
+++ src/bike/constants.py
@@ -7,6 +7,6 @@ DATA_DIR = os.path.join(BASE_PATH, 'data')
 STAGED_DATA_DIR = os.path.join(DATA_DIR, 'staged')
 SENT_DATA_DIR = os.path.join(DATA_DIR, 'sent')
 
-LOG_LOCATION = '/var/log/bike/bike.log'
+LOG_LOCATION = '/var/log/bike/' if os.getenv('TEST_ENV', 'False') == 'False' else '/tmp/log/bike'
 
 JOURNEY_SEND_FIELDS = {'uuid', 'data', 'transport_type', 'suspension'}
