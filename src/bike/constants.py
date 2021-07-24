b7f92a53d0b76bd1d58b0cc050dd08d3a4b36abd
Robert Lucey <robert@glimpse.ie>
2021-07-24 23:01:58 +0100

    Fix log location

--- src/bike/constants.py
+++ src/bike/constants.py
@@ -7,6 +7,6 @@ DATA_DIR = os.path.join(BASE_PATH, 'data')
 STAGED_DATA_DIR = os.path.join(DATA_DIR, 'staged')
 SENT_DATA_DIR = os.path.join(DATA_DIR, 'sent')
 
-LOG_LOCATION = '/var/log/bike/' if os.getenv('TEST_ENV', 'False') == 'False' else '/tmp/log/bike'
+LOG_LOCATION = '/var/log/bike/bike.log' if os.getenv('TEST_ENV', 'False') == 'False' else '/tmp/log/bike/bike.log'
 
 JOURNEY_SEND_FIELDS = {'uuid', 'data', 'transport_type', 'suspension'}
