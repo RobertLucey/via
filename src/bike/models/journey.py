153b9fe7102be3179db3aec2886323b03efb3b87
Robert Lucey <robert@glimpse.ie>
2021-07-24 23:06:37 +0100

    Fix save dir creation

--- src/bike/models/journey.py
+++ src/bike/models/journey.py
@@ -177,6 +177,11 @@ class Journey(Frames):
 
         filepath = os.path.join(STAGED_DATA_DIR, str(self.uuid) + '.json')
 
+        os.makedirs(
+            os.path.dirname(filepath),
+            exist_ok=True
+        )
+
         with open(filepath, 'w') as f:
             json.dump(
                 self.serialize(minimal=True),
