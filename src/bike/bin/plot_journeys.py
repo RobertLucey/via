739534b712fec99746dd775d6b71f70614ba12f4
Robert Lucey <robert@glimpse.ie>
2021-07-28 20:28:45 +0100

    Add plot journeys bin

---
+++ src/bike/bin/plot_journeys.py
@@ -0,0 +1,43 @@
+import argparse
+
+from bike.utils import get_journeys
+
+
+def main():
+    '''
+    Plot the map of journeys
+    '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--condition',
+        action='store_true',
+        dest='condition',
+        help='Show the condition of the roads'
+    )
+    parser.add_argument(
+        '--closest_edge',
+        action='store_true',
+        dest='closest_edge',
+        help='Use the closest route to the coordinates plotted on the actual journey. If no, similar journeys are not likely to overlap'
+    )
+    parser.add_argument(
+        '-m',
+        '--max_tries',
+        type=int,
+        default=5,
+        dest='max_tries',
+        help='Max times to attempt a single download.'
+    )
+    args = parser.parse_args()
+
+    journeys = get_journeys()
+    journeys.plot_routes(
+        use_closest_edge_from_base=args.closest_edge,
+        apply_condition_colour=args.condition,
+        plot_kwargs={'edge_linewidth': 5, 'show': True}
+    )
+
+
+if __name__ == '__main__':
+    main()
