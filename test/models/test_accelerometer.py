41a7b8560803678307b9b0b306872dacb426bc4b
Robert Lucey <robert@glimpse.ie>
2021-08-02 18:07:08 +0100

    Test accelerometer model

---
+++ test/models/test_accelerometer.py
@@ -0,0 +1,52 @@
+from unittest import TestCase
+
+from bike.models.accelerometer import AccelerometerPoint
+
+
+class AccelerometerPointTest(TestCase):
+
+
+    def test_serialize(self):
+        self.assertEqual(
+            AccelerometerPoint(1, None, 3).serialize(),
+            {
+                'x': 1,
+                'y': None,
+                'z': 3
+            }
+        )
+
+    def test_parse(self):
+        self.assertEqual(
+            AccelerometerPoint.parse(
+                (
+                    1,
+                    2,
+                    3
+                )
+            ).serialize(),
+            {
+                'x': 1,
+                'y': 2,
+                'z': 3
+            }
+        )
+
+        self.assertEqual(
+            AccelerometerPoint.parse(
+                {
+                    'x': 1,
+                    'y': 2,
+                    'z': 3
+                }
+            ).serialize(),
+            {
+                'x': 1,
+                'y': 2,
+                'z': 3
+            }
+        )
+
+    def test_is_populated(self):
+        self.assertTrue(AccelerometerPoint(1, 2, 3).is_populated)
+        self.assertFalse(AccelerometerPoint(1, None, 3).is_populated)
