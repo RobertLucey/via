3b2c450a4667607c5b58c649a6939e04bf77b074
Robert Lucey <robert@glimpse.ie>
2021-08-08 22:26:47 +0100

    Get remote files via lambda
    Don't want to have the modified date available

--- src/bike/bin/pull_journeys.py
+++ src/bike/bin/pull_journeys.py
@@ -1,21 +1,22 @@
 import os
 import boto3
 
+import requests
+
 from bike.constants import REMOTE_DATA_DIR
 
-s3 = boto3.client('s3', region_name='eu-west-1')
 
-paginator = s3.get_paginator('list_objects_v2')
+s3 = boto3.client('s3', region_name='eu-west-1')
 
 if not os.path.exists(REMOTE_DATA_DIR):
     os.makedirs(REMOTE_DATA_DIR, exist_ok=True)
 
-for page in paginator.paginate(Bucket='bike-road-quality'):
-    for file_object in page['Contents']:
-        local_fp = os.path.join(REMOTE_DATA_DIR, file_object['Key'])
-        if not os.path.exists(local_fp):
-            s3.download_file(
-                'bike-road-quality',
-                file_object['Key'],
-                local_fp
-            )
+for fn in requests.get('https://l7vv5djf9h.execute-api.eu-west-1.amazonaws.com/default/getUUIDs').json():
+
+    local_fp = os.path.join(REMOTE_DATA_DIR, fn)
+    if not os.path.exists(local_fp):
+        s3.download_file(
+            'bike-road-quality',
+            fn,
+            local_fp
+        )
