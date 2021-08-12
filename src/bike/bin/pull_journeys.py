d0d6aa1303ef1a085cd782da0432fc83d349dade
Robert Lucey <robert@glimpse.ie>
2021-08-12 21:19:17 +0100

    Add main to pull_journeys

--- src/bike/bin/pull_journeys.py
+++ src/bike/bin/pull_journeys.py
@@ -6,17 +6,22 @@ import requests
 from bike.constants import REMOTE_DATA_DIR
 
 
-s3 = boto3.client('s3', region_name='eu-west-1')
+def main():
+    s3 = boto3.client('s3', region_name='eu-west-1')
 
-if not os.path.exists(REMOTE_DATA_DIR):
-    os.makedirs(REMOTE_DATA_DIR, exist_ok=True)
+    if not os.path.exists(REMOTE_DATA_DIR):
+        os.makedirs(REMOTE_DATA_DIR, exist_ok=True)
 
-for fn in requests.get('https://l7vv5djf9h.execute-api.eu-west-1.amazonaws.com/default/getUUIDs').json():
+    for fn in requests.get('https://l7vv5djf9h.execute-api.eu-west-1.amazonaws.com/default/getUUIDs').json():
 
-    local_fp = os.path.join(REMOTE_DATA_DIR, fn)
-    if not os.path.exists(local_fp):
-        s3.download_file(
-            'bike-road-quality',
-            fn,
-            local_fp
-        )
+        local_fp = os.path.join(REMOTE_DATA_DIR, fn)
+        if not os.path.exists(local_fp):
+            s3.download_file(
+                'bike-road-quality',
+                fn,
+                local_fp
+            )
+
+
+if __name__ == '__main__':
+    main()
