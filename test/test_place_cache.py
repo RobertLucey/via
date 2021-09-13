883b0d4a14b40efccf94fb66f9a586a9c528cafc
Robert Lucey <robert@glimpse.ie>
2021-09-13 19:37:47 +0100

    Test get_by_bbox

--- test/test_place_cache.py
+++ test/test_place_cache.py
@@ -19,3 +19,42 @@ class PlaceCacheTest(TestCase):
             }
         )
         self.assertTrue('cork ireland' in cache.data)
+
+    def test_get_by_bbox_within(self):
+        cache = PlaceCache()
+        self.assertIsNotNone(
+            cache.get_by_bbox(
+                {
+                    'north': 53.61,
+                    'south': 53.4,
+                    'east': -6.2,
+                    'west': -6.3,
+                }
+            )
+        )
+
+    def test_get_by_bbox_exact(self):
+        cache = PlaceCache()
+        self.assertIsNotNone(
+            cache.get_by_bbox(
+                {
+                    'north': 53.626487,
+                    'south': 53.3018049,
+                    'east': -6.1366563,
+                    'west': -6.433500
+                }
+            )
+        )
+
+    def test_get_by_bbox_nowhere(self):
+        cache = PlaceCache()
+        self.assertIsNone(
+            cache.get_by_bbox(
+                {
+                    'north': 0,
+                    'south': 0,
+                    'east': 0,
+                    'west': 0,
+                }
+            )
+        )
