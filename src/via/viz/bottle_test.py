9d80e578ad0e9fd7ef9273c49d9d1c73e0d3fb7a
Conor Flynn <conor@glimpse.ie>
2021-09-21 22:19:08 +0100

    Supports pulling new journeys.

--- src/via/viz/bottle_test.py
+++ src/via/viz/bottle_test.py
@@ -1,10 +1,14 @@
-from via.constants import GEOJSON_DIR
 from via import logger
+
+from via.bin.pull_journeys import main as pull_journeys_main
+from via.bin.generate_geojson import main as generate_geojson_main
+from via.constants import GEOJSON_DIR
 from via.viz.dummy_data import full_journey
 
 import bottle
 import json
 import os
+import time
 
 
 @bottle.route('/hello')
@@ -23,9 +27,66 @@ def send_static(filename):
     return bottle.static_file(filename, root='static/')
 
 
-@bottle.route('/pull_journeys')
-def pull_journeys():
-    print("Pulling journeys")
+@bottle.route('/update_journeys')
+def update_journeys():
+    '''
+    Call the bin methods to pull new data and generate geojson from them.
+    TODO: Add throttling to not hit API limitations
+    TODO: Move the helper methods from bin
+    TODO: Accept GET params to only pull journeys of a particular type
+    '''
+    logger.info("Update journeys triggered")
+
+    # This is real dirty and I'm not happy with it...
+    # Should use a retry decorator in utils or similar if this has to be done.
+    retry_count = 3
+
+    for attempt_counter in range(retry_count):
+        try:
+            pull_journeys_main()
+            break
+        except Exception as e:
+            logger.error(f"Exception pulling journeys: {e}")
+
+            if attempt_counter == retry_count - 1:
+                logger.critical("Returning without pulling journeys.")
+                return {
+                    "message": "Pulling journeys failed",
+                    "status": 500
+                }
+            time.sleep(5)
+
+    logger.info("Journeys successfully pulled")
+
+    for attempt_counter in range(retry_count):
+        try:
+            generate_geojson_main()
+            break
+        except Exception as e:
+            logger.error(f"Exception converting journeys to geojson: {e}")
+
+            if attempt_counter == retry_count - 1:
+                logger.critical("Returning without converting journeys.")
+
+                return {
+                    "message": "Converting journeys failed",
+                    "status": 500
+                }
+            time.sleep(5)
+
+    logger.info("Journeys successfully parsed to geojson")
+
+    return {
+        "status": 200
+    }
+
+
+@bottle.route('/get_journeys')
+def get_journeys():
+    '''
+    API call to just get cached journeys. Use update_journeys to get new data.
+    '''
+    logger.info("Pulling cached journeys")
 
     earliest_time = bottle.request.query.earliest_time
     latest_time = bottle.request.query.latest_time
