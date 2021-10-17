b1cc14404419f573b12a7eab0c6357d81111e67e
Robert Lucey <robert@glimpse.ie>
2021-10-18 00:29:38 +0100

    Enable cors

--- src/via/bin/bottle_entry.py
+++ src/via/bin/bottle_entry.py
@@ -2,6 +2,7 @@ import argparse
 import threading
 
 import bottle
+from bottle import response
 
 from via import logger
 from via.pull_journeys import pull_journeys
@@ -11,6 +12,24 @@ from via.geojson.generate import generate_geojson
 from via.api import *
 from via.api.info import *
 from via.api.journeys import *
+
+
+class EnableCors(object):
+    name = 'enable_cors'
+    api = 2
+
+    def apply(self, fn, context):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+            if bottle.request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
 
 
 def update_journeys():
@@ -46,6 +65,7 @@ def main():
     )
 
     bottle.debug(args.debug)
+    bottle.install(EnableCors())
     bottle.run(
         host='0.0.0.0',
         port=args.port,
