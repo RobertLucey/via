c4bbccff4e223c071277eba422d1ae8c57a7ee3b
Robert Lucey <robert@glimpse.ie>
2021-10-18 17:40:33 +0100

    Debug to cyan

--- src/via/logging.py
+++ src/via/logging.py
@@ -11,6 +11,7 @@ FORMAT = '%(asctime)s|%(levelname)s| %(message)s'
 
 class ColourfulFormatter(logging.Formatter):
 
+    cyan = "\x1b[36m;21m"
     grey = "\x1b[38;21m"
     yellow = "\x1b[33;21m"
     red = "\x1b[31;21m"
@@ -20,7 +21,7 @@ class ColourfulFormatter(logging.Formatter):
     format_problematic = FORMAT + ' (%(filename)s:%(lineno)d)'
 
     FORMATS = {
-        logging.DEBUG: grey + format + reset,
+        logging.DEBUG: cyan + format + reset,
         logging.INFO: grey + format + reset,
         logging.WARNING: yellow + format_problematic + reset,
         logging.ERROR: red + format_problematic + reset,
