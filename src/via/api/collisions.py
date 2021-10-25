6ec1b9701831f5ce51d16c77a7730eee76fac654
Robert Lucey <robert@glimpse.ie>
2021-10-25 18:40:51 +0100

    Add filter to api

--- src/via/api/collisions.py
+++ src/via/api/collisions.py
@@ -11,9 +11,20 @@ COLLISIONS = get_collisions()
 def get_geojson():
     logger.info('Getting collision GeoJSON')
 
-    # TODO: filters
+    filters = {
+        'county': getattr(bottle.request.query, 'county', None),
+        'year': getattr(bottle.request.query, 'year', None),
+        'vehicle_type': getattr(bottle.request.query, 'vehicle_type', None)
+    }
 
-    filters = {'county': 'dublin'}
+    try:
+        filters['year'] = int(filters['year'])
+    except:
+        pass
+
+    # if None we don't want to get where the value is None, rather
+    # we don't care so remove it from the filter
+    filters = {k: v for k, v in filters.items() if v is not None and v != 'all'}
 
     dublin_collisions = COLLISIONS.filter(**filters)
     return dublin_collisions.geojson
