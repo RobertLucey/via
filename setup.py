886bce1a7f3ef6dab95c7f930aaffdf11c5b3fc2
Conor Flynn <conor-f@users.noreply.github.com>
2021-10-26 21:13:21 +0100

    Bumps to v0.0.8

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ INSTALL_REQUIRES = (
 
 setup(
     name='via-api',
-    version='0.0.7',
+    version='0.0.8',
     python_requires='>=3.6,<3.10',
     description='Analysing and serving crowdsourced road quality data',
     author='Robert Lucey',
