c12966ae7d36700bd3edfae2d3d16dba4fe8cc04
Robert Lucey <robert@glimpse.ie>
2021-10-26 23:19:30 +0100

    Improve base cache speed
    Distribute over many files based on 1st decimal point of lat and lng
    
    Will apply to network cache too, will speed things up a fair bit

--- src/via/base_cache.py
+++ src/via/base_cache.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import json
 
 from via import logger
 from via import settings
@@ -9,12 +10,13 @@ from via.utils import get_size
 
 class BaseCache():
 
-    def __init__(self, cache_type=None):
+    def __init__(self, cache_type=None, fn=None):
         assert cache_type is not None
         self.loaded = False
         self.data = {}
         self.last_save_len = -1
         self.cache_type = cache_type
+        self.fn = fn
 
     def get(self, k):
         if not self.loaded:
@@ -23,6 +25,8 @@ class BaseCache():
         return self.data.get(k, None)
 
     def set(self, k, v, skip_save=False):
+        if not self.loaded:
+            self.load()
         self.data[k] = v
         if not skip_save:
             self.save()
@@ -38,14 +42,14 @@ class BaseCache():
     def save(self):
         if len(self.data) <= self.last_save_len:
             return
-        logger.debug(f'Saving cache {self.cache_type}')
+        logger.info(f'Saving cache {self.fp}')
         self.create_dirs()
         with open(self.fp, 'wb') as f:
             pickle.dump(self.data, f)
         self.last_save_len = len(self.data)
 
     def load(self):
-        logger.debug(f'Loading cache {self.cache_type}')
+        logger.info(f'Loading cache {self.fp}')
         if not os.path.exists(self.fp):
             self.create_dirs()
             self.save()
@@ -56,9 +60,21 @@ class BaseCache():
         self.last_save_len = len(self.data)
         logger.debug(
             'Size of %s cache: %sMB',
-            self.cache_type,
+            self.fp,
             get_size(self.data) / (1000 ** 2)
         )
+
+    @staticmethod
+    def from_file(cache_type, filepath, load=True, fn=None):
+        cache = BaseCache(
+            cache_type=cache_type,
+            fn=os.path.basename(filepath)
+        )
+
+        if load:
+            cache.load()
+
+        return cache
 
     @property
     def dir(self) -> str:
@@ -66,4 +82,84 @@ class BaseCache():
 
     @property
     def fp(self) -> str:
-        return os.path.join(self.dir, 'cache.pickle')
+        return os.path.join(self.dir, f'{self.fn}')
+
+
+class BaseCaches():
+
+    def __init__(self, *args, **kwargs):
+        assert kwargs.get('cache_type', None) is not None
+        self.loaded = False
+        self.data = {}
+        self.last_save_len = -1
+        self.cache_type = kwargs['cache_type']
+        self.child_class = kwargs.get('child_class', BaseCache)
+
+        self.refs = {}
+        os.makedirs(
+            self.dir,
+            exist_ok=True
+        )
+        if not os.path.exists(self.refs_path):
+            with open(self.refs_path, 'w') as refs_file:
+                refs_file.write(json.dumps({}))
+        with open(self.refs_path, 'r') as refs_file:
+            self.refs = json.loads(refs_file.read())
+
+    def get(self, k):
+        # Have a header file or something
+
+        self.load()
+
+        if k in self.refs and self.refs[k] in self.data:
+            return self.data[self.refs[k]].get(k)
+
+        return None
+
+    def set(self, k, v, skip_save=False):
+        fn = '%s_%s.pickle' % (
+            round(min(v[0].geometry.x), 1),
+            round(min(v[0].geometry.y), 1)
+        )
+        if fn not in self.data:
+            self.data[fn] = self.child_class(fn=fn)
+        self.data[fn].set(k, v)
+        len_refs = len(self.refs)
+        self.refs[k] = fn
+        if len_refs < len(self.refs):
+            self.data[fn].save()
+            self.save_refs()
+
+    def save_refs(self):
+        with open(self.refs_path, 'w') as refs_file:
+            refs_file.write(json.dumps(self.refs))
+
+    def load(self):
+        if self.loaded:
+            return
+
+        try:
+            for c in self.caches:
+                self.data[c.fn] = c
+        except:
+            self.data = {}
+
+        self.loaded = True
+
+    @property
+    def dir(self):
+        return os.path.join(CACHE_DIR, self.cache_type, settings.VERSION)
+
+    @property
+    def caches(self):
+        return [
+            self.child_class.from_file(
+                self.cache_type,
+                os.path.join(self.dir, f),
+                load=False
+            ) for f in os.listdir(self.dir)
+        ]
+
+    @property
+    def refs_path(self):
+        return os.path.join(self.dir, 'refs.json')
--- src/via/bounding_graph_gdfs_cache.py
+++ src/via/bounding_graph_gdfs_cache.py
@@ -1,3 +1,20 @@
-from via.base_cache import BaseCache
+from via.base_cache import BaseCaches, BaseCache
 
-bounding_graph_gdfs_cache = BaseCache(cache_type='bounding_graph_gdfs_cache')
+
+class BoundingGraphGDFSGraph(BaseCache):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['cache_type'] = 'bounding_graph_gdfs_cache'
+        super().__init__(*args, **kwargs)
+
+
+class BoundingGraphGDFSGraphs(BaseCaches):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['child_class'] = BoundingGraphGDFSGraph
+        kwargs['cache_type'] = 'bounding_graph_gdfs_cache'
+        super().__init__(*args, **kwargs)
+
+
+bounding_graph_gdfs_cache = BoundingGraphGDFSGraphs()
+bounding_graph_gdfs_cache.load()
