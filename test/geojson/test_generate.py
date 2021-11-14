db94a11bd7ddc0c652a4c1cbf4351bc68c1c2c4e
Robert Lucey <robert@glimpse.ie>
2021-11-14 14:24:13 +0000

    Add some todo tests

---
+++ test/geojson/test_generate.py
@@ -0,0 +1,17 @@
+from unittest import TestCase, skip
+
+from via.geojson.generate import (
+    get_generation_config,
+    generate_geojson
+)
+
+
+class GeoJsonGenerateTest(TestCase):
+
+    @skip('todo')
+    def test_get_generation_config(self):
+        pass
+
+    @skip('todo')
+    def test_generate_geojson(self):
+        pass
---
+++ test/geojson/test_retrieve.py
@@ -0,0 +1,10 @@
+from unittest import TestCase, skip
+
+from via.geojson.retrieve import get_geojson
+
+
+class GeoJsonRetrieveTest(TestCase):
+
+    @skip('todo')
+    def test_get_geojson(self):
+        pass
