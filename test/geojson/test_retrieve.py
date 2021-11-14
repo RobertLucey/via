d8f213672bd3b4a33fe74072019ba7bf0e90623d
Robert Lucey <robert@glimpse.ie>
2021-11-14 14:45:49 +0000

    Basic test and more skips

--- test/geojson/test_retrieve.py
+++ test/geojson/test_retrieve.py
@@ -5,6 +5,16 @@ from via.geojson.retrieve import get_geojson
 
 class GeoJsonRetrieveTest(TestCase):
 
+    def test_get_geojson_not_exist(self):
+        with self.assertRaises(FileNotFoundError):
+            get_geojson(
+                'blahblah'
+            )
+
     @skip('todo')
-    def test_get_geojson(self):
+    def test_get_geojson_does_exist_too_old(self):
+        pass
+
+    @skip('todo')
+    def test_get_geojson_does_exist_good_age(self):
         pass
