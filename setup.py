7fe9f50228743ee3f7aefa125c25a516c98ad061
Robert Lucey <robert@glimpse.ie>
2021-11-25 19:15:40 +0000

    Bump to 0.0.44

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ INSTALL_REQUIRES = (
 
 setup(
     name='via-api',
-    version='0.0.43',
+    version='0.0.44',
     python_requires='>=3.6,<3.10',
     description='Analysing and serving crowdsourced road quality data',
     author='Robert Lucey',
