e50f8cb7f9eb1e857d5ac71b65605b1a7591fd25
Robert Lucey <robert@glimpse.ie>
2022-08-01 18:31:14 +0100

    Min speed to avg walking speed

--- src/via/settings.py
+++ src/via/settings.py
@@ -11,7 +11,7 @@ PREPARED_CACHE_BUCKET = os.getenv('PREPARED_CACHE_BUCKET', 'via-cache')
 
 MIN_ACC_SCORE = float(os.getenv('MIN_ACC_SCORE', '0.001'))
 MIN_PER_JOURNEY_USAGE = float(os.getenv('MIN_PER_JOURNEY_USAGE', '1'))
-MIN_METRES_PER_SECOND = float(os.getenv('MIN_METRES_PER_SECOND', '2'))
+MIN_METRES_PER_SECOND = float(os.getenv('MIN_METRES_PER_SECOND', '1.4'))
 MAX_METRES_PER_SECOND = float(os.getenv('MAX_METRES_PER_SECOND', '10000'))
 
 MIN_JOURNEY_VERSION = version.parse(os.getenv('MIN_JOURNEY_VERSION', '0.0.0'))
@@ -23,7 +23,7 @@ MAX_JOURNEY_VERSION = version.parse(os.getenv('MAX_JOURNEY_VERSION', '999.999.99
 GPS_INCLUDE_RATIO = int(os.getenv('GPS_INCLUDE_RATIO', '2'))
 
 NEAREST_EDGE_METHOD = os.getenv('NEAREST_EDGE_METHOD', 'angle_nearest')
-CLOSE_ANGLE_TO_ROAD = float(os.getenv('CLOSE_ANGLE_TO_ROAD', 15))
+CLOSE_ANGLE_TO_ROAD = float(os.getenv('CLOSE_ANGLE_TO_ROAD', 20))
 
 DEFAULT_OVERPASS_API = os.getenv('DEFAULT_OVERPASS_API', 'https://overpass-api.de/api')
 CUSTOM_OVERPASS_API = os.getenv('CUSTOM_OVERPASS_API', 'http://54.73.95.15/api')
