a2118a561b96dad9ca0e1052049ff9cd7c8f569a
Robert Lucey <robert@glimpse.ie>
2022-08-01 20:45:51 +0100

    Geojson generated file cache to one hour

--- src/via/settings.py
+++ src/via/settings.py
@@ -39,6 +39,6 @@ CLEAN_MEMORY = os.getenv("TEST_ENV", "False") == "False"
 
 MAX_JOURNEY_METRES_SQUARED = 5e7  # 50km^2
 
-MAX_GEOJSON_AGE = 60 * 60 * 12
+MAX_GEOJSON_AGE = 60 * 60
 
 VERSION = pkg_resources.require("via-api")[0].version
