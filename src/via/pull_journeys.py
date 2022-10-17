1ecd5e9c5b1bc564d00efdda6f41cc8998c3c6d5
Robert Lucey <robert@glimpse.ie>
2022-10-17 18:37:55 +0100

    Multiprocess pull_journeys

--- src/via/pull_journeys.py
+++ src/via/pull_journeys.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import glob
+import multiprocessing
 
 import boto3
 import requests
@@ -13,12 +14,56 @@ from via.constants import REMOTE_DATA_DIR
 from via.models.journey import Journey
 
 
+S3_CLIENT = None
+
+
+def download_journey(data):
+    if S3_CLIENT is None:
+        s3 = boto3.client(
+            "s3", region_name=S3_REGION, config=Config(signature_version=UNSIGNED)
+        )
+
+    filename = data["filename"]
+    log_geo = data["log_geo"]
+    cache_graphs = data["cache_graphs"]
+
+    journey_id = os.path.splitext(filename)[0]
+
+    tmp_filepath = f"/tmp/{journey_id}.json"
+
+    s3.download_file("bike-road-quality", filename, tmp_filepath)
+
+    journey = Journey.from_file(tmp_filepath)
+
+    if not journey.has_enough_data:
+        logger.warning(f"Journey {journey_id} has not enough data")
+        return
+
+    if log_geo:
+        geo = journey.origin.gps.reverse_geo
+        logger.info(
+            f'Pulled journey from: {geo["cc"]}, {geo["place_3"]}, {geo["place_2"]}, {geo["place_1"]}'
+        )
+
+    if cache_graphs:
+        logger.info(f"Caching graphs for {journey_id}")
+        journey.bounding_graph
+
+    local_filepath = os.path.join(
+        REMOTE_DATA_DIR, journey.transport_type.lower(), filename
+    )
+    logger.info(f"Putting to {local_filepath}")
+
+    os.makedirs(os.path.dirname(local_filepath), exist_ok=True)
+
+    shutil.move(tmp_filepath, local_filepath)
+
+
 def get_journey_files():
     return requests.get(DOWNLOAD_JOURNEYS_URL).json()
 
 
 def get_journey_files_to_download():
-
     journey_ids = []
     for filename in glob.iglob(REMOTE_DATA_DIR + "/**/*", recursive=True):
         journey_ids.append(os.path.splitext(os.path.basename(filename))[0])
@@ -41,9 +86,6 @@ def pull_journeys(cache_graphs=False, log_geo=False):
     :log_geo: to log geographical data or not. Logging this slows down
         the pulling
     """
-    s3 = boto3.client(
-        "s3", region_name=S3_REGION, config=Config(signature_version=UNSIGNED)
-    )
 
     if not os.path.exists(REMOTE_DATA_DIR):
         os.makedirs(REMOTE_DATA_DIR, exist_ok=True)
@@ -52,35 +94,11 @@ def pull_journeys(cache_graphs=False, log_geo=False):
 
     logger.info(f"Downloading {len(journey_files_to_download)} files")
 
-    for filename in journey_files_to_download:
-
-        journey_id = os.path.splitext(filename)[0]
-
-        tmp_filepath = f"/tmp/{journey_id}.json"
-
-        s3.download_file("bike-road-quality", filename, tmp_filepath)
-
-        journey = Journey.from_file(tmp_filepath)
-
-        if not journey.has_enough_data:
-            logger.warning(f"Journey {journey_id} has not enough data")
-            continue
-
-        if log_geo:
-            geo = journey.origin.gps.reverse_geo
-            logger.info(
-                f'Pulled journey from: {geo["cc"]}, {geo["place_3"]}, {geo["place_2"]}, {geo["place_1"]}'
-            )
-
-        if cache_graphs:
-            logger.info(f"Caching graphs for {journey_id}")
-            journey.bounding_graph
-
-        local_filepath = os.path.join(
-            REMOTE_DATA_DIR, journey.transport_type.lower(), filename
+    with multiprocessing.Pool(multiprocessing.cpu_count() - 1) as pool:
+        pool.map(
+            download_journey,
+            [
+                {"filename": filename, "cache_graphs": cache_graphs, "log_geo": log_geo}
+                for filename in journey_files_to_download
+            ],
         )
-        logger.info(f"Putting to {local_filepath}")
-
-        os.makedirs(os.path.dirname(local_filepath), exist_ok=True)
-
-        shutil.move(tmp_filepath, local_filepath)
