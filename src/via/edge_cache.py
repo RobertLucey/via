76a4c90239bf726290fec40a1c86807ef66aaee9
Robert Lucey <robert.lucey@visionr.com>
2023-08-07 12:35:56 +0100

    Potentially faster edge_cache

--- src/via/edge_cache.py
+++ src/via/edge_cache.py
@@ -11,15 +11,14 @@ def get_edge_data(origin_uuid: str, destination_uuid: str, graph=None):
     :param destination_uuid: osmid of the destination node
     :return: TODO: can't remember exactly what gets returned
     """
+    if graph is None:
+        # Since we can provide no more context
+        return None
 
     combined_id = get_combined_id(origin_uuid, destination_uuid)
 
     try:
         return EDGE_CACHE[combined_id]
     except KeyError:
-        if graph is None:
-            # Since we can provide no more context
-            return None
-
         EDGE_CACHE[combined_id] = graph.get_edge_data(origin_uuid, destination_uuid)
         return EDGE_CACHE[combined_id]
