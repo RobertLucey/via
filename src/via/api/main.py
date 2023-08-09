b8db9d87e9c25aaa978d04969e9d31af54891653
Robert Lucey <robert.lucey@visionr.com>
2023-08-09 09:18:26 +0100

    Generate geojson every 6 hours

--- src/via/api/main.py
+++ src/via/api/main.py
@@ -130,7 +130,7 @@ async def get_all_journeys(
 def start_geojson_generate(first_run):
     from via.geojson import generate
 
-    threading.Timer(5, start_geojson_generate, (False,)).start()
+    threading.Timer(60 * 60 * 6, start_geojson_generate, (False,)).start()
     if not first_run:
         # Don't do on startup
         generate.generate_geojson(
