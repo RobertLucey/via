c1013d173c8edd0e1e03b7aad3b86042a9e6d486
Robert Lucey <robert.lucey@visionr.com>
2023-09-01 18:36:01 +0100

    Improve journey puller

--- src/via/bin/pull_external_journeys.py
+++ src/via/bin/pull_external_journeys.py
@@ -7,9 +7,7 @@ from via.db import db
 from via.models.journey import Journey
 
 
-URLS = [
-    # "http://localhost:8000"
-]
+URLS = ["https://via-api.randombits.host"]
 
 
 def main():
@@ -21,28 +19,28 @@ def main():
         journeys_uuids = requests.get(query_url).json()
 
         for journey_uuid in journeys_uuids:
-            print(journey_uuid)
-
-            query_string = urlencode(OrderedDict(journey_uuid=journey_uuid))
-            url = f"{base_url}/get_raw_journey?{query_string}"
-
-            journey_data = requests.get(url).json()
-
-            # Validate the journey
-            Journey(
-                uuid=journey_data["uuid"],
-                data=journey_data["data"],
-                transport_type=journey_data["transport_type"],
-                suspension=journey_data["suspension"],
-                version=journey_data["version"],
-            )
-
-            result = db.raw_journeys.find_one({"uuid": journey_data["uuid"]})
+            result = db.raw_journeys.find_one({"uuid": journey_uuid})
 
             if not result:
+                print(journey_uuid)
+
+                query_string = urlencode(OrderedDict(journey_uuid=journey_uuid))
+                url = f"{base_url}/get_raw_journey?{query_string}"
+
+                journey_data = requests.get(url).json()
+
+                # Validate the journey
+                Journey(
+                    uuid=journey_data["uuid"],
+                    data=journey_data["data"],
+                    transport_type=journey_data["transport_type"],
+                    suspension=journey_data["suspension"],
+                    version=journey_data["version"],
+                )
+
                 db.raw_journeys.insert_one(journey_data)
             else:
-                print(f'already exists: {journey_data["uuid"]}')
+                print(f"already exists: {journey_uuid}")
 
 
 if __name__ == "__main__":
